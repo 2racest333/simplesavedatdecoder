3359e98304140ea55e67dd34a4701f3a9de7b8fc
Racest <63019122+2racest333@users.noreply.github.com>
2020-12-31 21:00:07 +0300

    Add files via upload

---
+++ simplesavedatadecryptorbyracest/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace simplesavedatadecryptorbyracest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.markAsHackedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.growid = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.savedats = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(401, 294);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Hacked";
+            this.columnHeader1.Width = 70;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "         GrowID";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "      Passwords";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Filename";
+            this.columnHeader4.Width = 70;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.markAsHackedToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 26);
+            // 
+            // markAsHackedToolStripMenuItem
+            // 
+            this.markAsHackedToolStripMenuItem.Name = "markAsHackedToolStripMenuItem";
+            this.markAsHackedToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.markAsHackedToolStripMenuItem.Text = "Mark as hacked";
+            this.markAsHackedToolStripMenuItem.Click += new System.EventHandler(this.markAsHackedToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(443, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(186, 248);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(439, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GrowID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(439, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Passwords:";
+            // 
+            // growid
+            // 
+            this.growid.AutoSize = true;
+            this.growid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.growid.ForeColor = System.Drawing.Color.White;
+            this.growid.Location = new System.Drawing.Point(502, 12);
+            this.growid.Name = "growid";
+            this.growid.Size = new System.Drawing.Size(0, 20);
+            this.growid.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "REFRESH FOLDER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // savedats
+            // 
+            this.savedats.Location = new System.Drawing.Point(275, 322);
+            this.savedats.Name = "savedats";
+            this.savedats.ReadOnly = true;
+            this.savedats.Size = new System.Drawing.Size(371, 20);
+            this.savedats.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 19);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(99, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 39);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "CLOSE ALL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(150, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 39);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "SHOW NON-HACKEDS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(652, 359);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.savedats);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.growid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "easy savedata decryptor made by racest#0755 (too lazy to make better design)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label growid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem markAsHackedToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox savedats;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ simplesavedatadecryptorbyracest/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace simplesavedatadecryptorbyracest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class PasswordDec
+        {
+            public List<string> PPW(byte[] contents)
+            {
+                List<string> result;
+                try
+                {
+                    string text = "";
+                    for (int i = 0; i < contents.Length; i += 1)
+                    {
+                        byte b = contents[i];
+                        string text2 = b.ToString("X2");
+                        bool flag = text2 == "00";
+                        if (flag)
+                        {
+                            text += "<>";
+                        }
+                        else
+                        {
+                            text += text2;
+                        }
+                    }
+                    bool flag2 = text.Contains("74616E6B69645F70617373776F7264");
+                    if (flag2)
+                    {
+                        string text3 = "74616E6B69645F70617373776F7264";
+                        int num = text.IndexOf(text3);
+                        int num2 = text.LastIndexOf(text3);
+                        bool flag3 = false;
+                        string text4;
+                        if (flag3)
+                        {
+                            text4 = string.Empty;
+                        }
+                        num += text3.Length;
+                        int num3 = text.IndexOf("<><><>", num);
+                        bool flag4 = false;
+                        if (flag4)
+                        {
+                            text4 = string.Empty;
+                        }
+
+                        string @string = Encoding.UTF8.GetString(StringToByteArray(text.Substring(num, num3 - num).Trim()));
+                        bool flag5 = ((@string.ToCharArray()[0] == 95) ? 1 : 0) == 0;
+                        if (flag5)
+                        {
+                            text4 = text.Substring(num, num3 - num).Trim();
+                        }
+                        else
+                        {
+                            num2 += text3.Length;
+                            num3 = text.IndexOf("<><><>", num2);
+                            text4 = text.Substring(num2, num3 - num2).Trim();
+                        }
+                        string text5 = "74616E6B69645F70617373776F7264" + text4 + "<><><>";
+                        int num4 = text.IndexOf(text5);
+                        bool flag6 = false;
+                        string text6;
+                        if (flag6)
+                        {
+                            text6 = string.Empty;
+                        }
+                        num4 += text5.Length;
+                        int num5 = text.IndexOf("<><><>", num4);
+                        bool flag7 = false;
+                        if (flag7)
+                        {
+                            text6 = string.Empty;
+                        }
+
+                        text6 = text.Substring(num4, num5 - num4).Trim();
+                        int num6 = StringToByteArray(text4)[0];
+                        text6 = text6.Substring(0, num6 * 2);
+                        byte[] array = StringToByteArray(text6.Replace("<>", "00"));
+                        List<byte> list = new List<byte>();
+                        List<byte> list2 = new List<byte>();
+                        byte b2 = (byte)(48 - array[0]);
+                        byte[] array2 = array;
+                        for (int j = 0; j < array2.Length; j += 1)
+                        {
+                            byte b3 = array2[j];
+                            list.Add((byte)(b2 + b3));
+                        }
+                        for (int k = 0; k < list.Count; k += 1)
+                        {
+                            list2.Add((byte)(list[k] - 1 - k));
+                        }
+                        List<string> list3 = new List<string>();
+                        int num7 = 0;
+                        while ((num7 > 255 ? 1 : 0) == 0)
+                        {
+                            string text7 = "";
+                            foreach (byte b4 in list2)
+                            {
+                                bool flag8 = ValidateChar((char)((byte)((int)b4 + num7)));
+                                if (flag8)
+                                {
+                                    text7 += ((char)((byte)((int)b4 + num7))).ToString();
+                                }
+                            }
+                            bool flag9 = text7.Length == num6;
+                            if (flag9)
+                            {
+                                list3.Add(text7);
+                            }
+                            num7 += 1;
+                        }
+                        result = list3;
+                    }
+                    else
+                    {
+                        result = null;
+                    }
+                }
+                catch
+                {
+                    result = null;
+                }
+                return result;
+            }
+            public byte[] StringToByteArray(string str)
+            {
+                Dictionary<string, byte> hexindex = new Dictionary<string, byte>();
+                for (int i = 0; i <= 255; i++)
+                    hexindex.Add(i.ToString("X2"), (byte)i);
+
+                List<byte> hexres = new List<byte>();
+                for (int i = 0; i < str.Length; i += 2)
+                    hexres.Add(hexindex[str.Substring(i, 2)]);
+
+                return hexres.ToArray();
+            }
+            private bool ValidateChar(char cdzdshr)
+            {
+                if ((cdzdshr >= 0x30 && cdzdshr <= 0x39) ||
+                        (cdzdshr >= 0x41 && cdzdshr <= 0x5A) ||
+                        (cdzdshr >= 0x61 && cdzdshr <= 0x7A) ||
+                        (cdzdshr >= 0x2B && cdzdshr <= 0x2E)) return true;
+                else return false;
+            }
+
+            public string[] Func(byte[] lel)
+            {
+                byte[] buff = lel;
+                var passwords = PPW(buff);
+                return passwords.ToArray();
+            }
+        }
+        Random rastgele = new Random();
+        public string savedataspath;
+        bool showonlynhacked = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (savedataspath == null)
+            {
+                MessageBox.Show("please select folder that contains your savedats.");
+                return;
+            }
+            var pattern = new Regex(@"[^\w0-9]");
+            string contents;
+            int failedcount = 0;
+            foreach (string file in Directory.EnumerateFiles(savedataspath, "*.dat"))
+            {
+                try
+                {
+                    string status = "FALSE";
+                    string filename = Path.GetFileName(file);
+                    if (filename.Contains("hackedtrue"))
+                        status = "TRUE";
+                    var r = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    using (FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        using (StreamReader streamReader = new StreamReader(fileStream, Encoding.Default))
+                        {
+                            contents = streamReader.ReadToEnd();
+                        }
+                    }
+                    string cleardata = contents.Replace("\u0000", " ");
+                    string growid = pattern.Replace(cleardata.Substring(cleardata.IndexOf("tankid_name") + "tankid_name".Length).Split(' ')[3], string.Empty);
+                    string[] passwords = new PasswordDec().Func(Encoding.Default.GetBytes(contents));
+                    string str1 = "";
+                    foreach (string str3 in passwords)
+                        str1 = str1 + str3 + Environment.NewLine;
+                    string[] arr1 = new string[4];
+                    arr1[0] = status;
+                    arr1[1] = growid;
+                    arr1[2] = str1;
+                    arr1[3] = filename;
+                    ListViewItem itm1;
+                    itm1 = new ListViewItem(arr1);
+                    listView1.Items.Add(itm1);
+                }
+                catch { ++failedcount; }
+            }
+            if (failedcount != 0 )
+                MessageBox.Show("failed count " + failedcount);
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+            ListViewItem item = listView1.SelectedItems[0];
+            richTextBox1.Text = item.SubItems[2].Text;
+            growid.Text = item.SubItems[1].Text;
+        }
+
+        private void markAsHackedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int sayi = rastgele.Next(10, 100000);
+            if (listView1.SelectedItems.Count == 0)
+                return;
+            ListViewItem item = listView1.SelectedItems[0];
+            if (item.SubItems[0].Text == "FALSE")
+            {
+                try
+                {
+                    File.Move((savedataspath + "/" + item.SubItems[3].Text), (savedataspath + "/" + sayi + "hackedtrue.dat"));
+                    item.SubItems[0].Text = "TRUE";
+                    if (showonlynhacked)
+                    item.ForeColor = Color.Black;
+                }
+                catch { MessageBox.Show("error while marking account as hacked."); }
+            }
+            else
+                item.SubItems[0].Text = "FALSE";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            savedats.Text = savedataspath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Custom Description";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                savedataspath = fbd.SelectedPath;
+                savedats.Text = savedataspath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                if (item.SubItems[0].Text == "TRUE")
+                {
+                    if (!showonlynhacked)
+                    {
+                        item.ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                         item.ForeColor = Color.White;
+                    }
+                }
+                }
+            if (showonlynhacked == false)
+                showonlynhacked = true;
+            else
+                showonlynhacked = false;
+        }
+        }
+    }
